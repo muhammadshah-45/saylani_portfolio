6ced65885fea1c364339cc76d8d71979a31670b6
muhammadshah-45 <mshahaurangzeb45@gmail.com>
2024-09-19 20:58:13 +0500

    I style hero section with responsiveness

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 var hamburger = document.querySelector(".hamburger");
 var navlist= document.querySelector(".nav-list ul");
+var menuItem= document.querySelectorAll(".nav-list ul li a");
+var header = document.querySelector("header")
+console.log(navlist);
 hamburger.addEventListener("click",()=>{
     hamburger.classList.toggle("active");
     navlist.classList.toggle("active")
 });
 
 
+menuItem.forEach(item => {
+    item.addEventListener("click",()=>{
+        hamburger.classList.toggle("active");
+        navlist.classList.toggle("active")
+    })
+});
+
+document.addEventListener("scroll",()=>{
+    var scroll_position = window.screenY;
+    if(scroll_position > 250){
+        header.style.backgroundColor = "red"
+
+    }else{
+        header.style.backgroundColor= "green"
+    }
+})
